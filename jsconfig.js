4c7beeb0e3398dfabd60ce1fa778bf761b525694
Druid1984 <124631432+Druid1984@users.noreply.github.com>
2024-10-16 17:15:14 +0100

    added files via upload
    portfolio github page creation

---
+++ jsconfig.js
@@ -0,0 +1,11 @@
+{
+  "compilerOptions": {
+    "target": "es6",
+    "module": "commonjs",
+    "baseUrl": "./src",
+    "paths": {
+      "*": ["node_modules/*"]
+    }
+  },
+  "include": ["src/**/*"]
+}
